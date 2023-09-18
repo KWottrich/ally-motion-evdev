4671d609c07693f22375153e186772599ac57757
KWottrich <kenny.wottrich@gmail.com>
2023-09-17 20:48:38 -0500

    flush debug

--- main.cpp
+++ main.cpp
@@ -90,6 +90,7 @@ int main(int argc, char** argv)
 		gyroFrame.scale(GYRO_SCALE);
 		std::cout<<"\33[2K\r";
 		printf("%+4.9f | %+4.9f | %+4.9f", gyroFrame.x, gyroFrame.y, gyroFrame.z);
+		std::cout<<std::flush;
 		if (abs(gyroFrame.x) > maxGyroX)
 			maxGyroX = abs(gyroFrame.x);
 		if (abs(gyroFrame.y) > maxGyroY)
