86c87f3bdbaf0805bdde97628fb67e592f48b510
KWottrich <kenny.wottrich@gmail.com>
2023-09-17 20:35:33 -0500

    cleaner debug output

--- main.cpp
+++ main.cpp
@@ -88,7 +88,7 @@ int main(int argc, char** argv)
 		
 		Gyro::Frame gyroFrame = gyro.getFrame();
 		gyroFrame.scale(GYRO_SCALE);
-		std::cout<<"\r";
+		std::cout<<"\33[2K\r";
 		printf("%+4.9f | %+4.9f | %+4.9f", gyroFrame.x, gyroFrame.y, gyroFrame.z);
 		if (abs(gyroFrame.x) > maxGyroX)
 			maxGyroX = abs(gyroFrame.x);
